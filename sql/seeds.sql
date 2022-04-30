8dd2e3a4484099ab90f5863959834078914ecf8b
Jake Walsh <jkwalsh127@gmail.com>
2022-04-29 19:43:38 -0700

    seed database

---
+++ sql/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO departments
+(department_name) VALUES
+    ("Booking");
+
+INSERT INTO roles
+(title, salary, department_id) VALUES
+-- check on using decimals
+    ("engineer", .100, 1);
+
+INSERT INTO employees
+(first_name, last_name, role_id, manager_id) VALUES
+    ("john", "doe", 1, 2);
