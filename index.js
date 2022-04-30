7a0952c6688b86688d57c417e18c127eff43f0e3
Jake Walsh <jkwalsh127@gmail.com>
2022-04-29 20:37:37 -0700

    include classes in query functions to validate user inputs

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mysql = require('mysql2');
 const fs = require('fs');
 let prompts = require('./src/prompts');
 const { addDepartment } = require('./src/prompts');
+const departmentClass = require('./lib/departments');
+const roleClass = require('./lib/roles');
+const employeeClass = require('./lib/employees');
+const updateClass = require('./lib/updates');
 
 /**
  * the initiator function uses inquirer to begin calling the prompts
@@ -55,6 +59,7 @@ function viewEmployees() {
 }
 
 function addDepartment(answers) {
+    let newDepartment = new departmentClass(answers.departmentName);
     db.query("INSERT INTO departments (department_name) VALUES (?);", answers.departmentName, (err, result) => {
         if (err) {
             console.log(err);
@@ -63,6 +68,7 @@ function addDepartment(answers) {
     });
 };
 function addRole(answers) {
+    let newRole = new roleClass(answers.roleName, answers.roleSalary, answers.roleDepartment);
     db.query("INSERT INTO roles (title, salary, department_id) VALUES (?);", [ answers.roleName, answers.roleSalary, answers.roleDepartment ] , (err, result) => {
         if (err) {
             console.log(err);
@@ -71,6 +77,7 @@ function addRole(answers) {
     });
 };
 function addEmployee(answers) {
+    let newEmployee = new employeeClass(answers.employeeFirstName, answers.employeeLastName, answers.employeeRole, answers.employeeManager);
     db.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?);", [answers.employeeFirstName, answers.employeeLastName, answers.employeeRole, answers.employeeManager ], (err, result) => {
         if (err) {
             console.log(err);
@@ -79,6 +86,7 @@ function addEmployee(answers) {
     });
 };
 function updateRole(answers) {
+    let updateRole = new updateClass(answers.newRole, answers.employeeID);
     db.query("UPDATE employees SET role_id = ? WHERE id = ?;", [answers.newRole, answers.employeeID] , (err, result) => {
         if (err) {
             console.log(err);
